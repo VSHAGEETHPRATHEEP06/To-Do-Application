100ca6b7afdb597282b3a635716580f251472e88
VSHAGEETHPRATHEEP06 <shagith06@gmail.com>
2025-04-01 00:03:13 +0530

    IP address fix

--- frontend/src/Axios/axios.js
+++ frontend/src/Axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios"
 const instance = axios.create({
-    baseURL:"http://13.250.16.170:8000/api"
+    baseURL: import.meta.env.VITE_API_URL || "http://13.250.16.170:8000/api"
 })
 export default instance
